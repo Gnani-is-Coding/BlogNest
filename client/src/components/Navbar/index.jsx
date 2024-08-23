349ae77de0290aec9fabae8fe6fcc5f7e1aafc90
Gnani-is-Coding <gnanendragariminti@nexocide.com>
2024-08-23 13:08:13 +0530

    adds navbar

---
+++ client/src/components/Navbar/index.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import "./index.css";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar-container">
+        <Link to="/" className="heading">
+          BlogNest
+        </Link>
+
+        <div className="btn-containers">
+          <button className="login-btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-sun-moon"
+            >
+              <path d="M12 8a2.83 2.83 0 0 0 4 4 4 4 0 1 1-4-4" />
+              <path d="M12 2v2" />
+              <path d="M12 20v2" />
+              <path d="m4.9 4.9 1.4 1.4" />
+              <path d="m17.7 17.7 1.4 1.4" />
+              <path d="M2 12h2" />
+              <path d="M20 12h2" />
+              <path d="m6.3 17.7-1.4 1.4" />
+              <path d="m19.1 4.9-1.4 1.4" />
+            </svg>
+            {/* <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-moon"
+            >
+              <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+            </svg> */}
+          </button>
+          <button className="login-btn">
+            Login
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-log-in"
+            >
+              <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
+              <polyline points="10 17 15 12 10 7" />
+              <line x1="15" x2="3" y1="12" y2="12" />
+            </svg>
+          </button>
+          <button className="login-btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-square-plus"
+            >
+              <rect width="18" height="18" x="3" y="3" rx="2" />
+              <path d="M8 12h8" />
+              <path d="M12 8v8" />
+            </svg>
+            Join Now
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
